f162fb027564eb22e4deb48d8d2ba14858107cc7
PeterTing <H34036047@2015cpp.(none)>
2015-05-26 23:02:28 +0800

    sorry I give the wrong version....

--- Sudoku.h
+++ Sudoku.h
@@ -1,15 +1,39 @@
 #include<iostream>
 #include<vector>
+#include<fstream>
+#include<iomanip>
+#include<cstdlib>
+#include<algorithm>
+#include<ctime>
+using namespace std;
+struct doublepos{
+	int position;
+	vector<int> possibility;
+};
 class Sudoku{
 public:
 	Sudoku();
-	void findAnswer();
-	void setMap(vector<const int> set_map);
-	int getElement[int index];
-	bool correct();
+	void solve();
+	void setMap(vector<int> set_map);
+	void scanPossibility();
+	void printMap();
+	void giveQuestion();
+	void Read_In();
+	bool isCorrect();
+	bool give();
 	static const int sudokuSize=144;
+	int* map;
+	int* giveMap;
 private:
 	bool checkUnity(vector<int> arr);
-	vector<int> map(sudokuSize);
+	
+	vector<int>* countrow;
+	vector<int>* countcol;
+	vector<int>* countcel;
+	vector<vector<int> >* saverow;
+	vector<vector<int> >* savecol;
+	vector<vector<int> >* savecel;
+	vector<doublepos*> possible[10];
+	vector<int>* randNum;
+	int location;
 };
-
